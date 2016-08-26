323dcbd131c53bdefbee1c4c28aa37a434459841
Ondrej Brinkel <info@anzui.de>
2016-08-27 00:39:51 +0200

    initial commit

---
+++ lang/de/lang.php
@@ -0,0 +1,7 @@
+<?php
+return [
+  'plugin.login' => 'Als Benutzer anmelden',
+  'success' => 'Du bist nun als ":username" angemeldet. Bitte sei vorsichtig mit dem was du tust.',
+  'error' => 'Da ist was schief gegangen.',
+  'back' => 'Zurück'
+];
---
+++ lang/en/lang.php
@@ -0,0 +1,7 @@
+<?php
+return [
+  'plugin.login' => 'Login as User',,
+  'success' => 'You are now logged in as ":username". Be carefull with what you are doing.',
+  'error' => 'Something went wrong.',
+  'back' => 'Back',
+];
