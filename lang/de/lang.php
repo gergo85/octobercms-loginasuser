4b4f165e5548e4b7ddf9bcbe57a0da88e697cfe3
Ondrej Brinkel <info@anzui.de>
2016-08-29 15:29:29 +0200

    translated button

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -3,5 +3,6 @@ return [
   'plugin.login' => 'Als Benutzer anmelden',
   'success' => 'Du bist nun als ":username" angemeldet. Bitte sei vorsichtig mit dem was du tust.',
   'error' => 'Da ist was schief gegangen.',
-  'back' => 'Zurück'
+  'back' => 'Zurück',
+  'loginasuser' => 'Als dieser Benutzer anmelden',
 ];
